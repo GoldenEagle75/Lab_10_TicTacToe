c0060def9dec75b4f9c8542a7825994daea65dde
GoldenEagle75 <marshall.muhlhauser@gmail.com>
2023-07-04 17:21:33 -0400

    Lab Completed

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class TicTacToe {
     private static final int ROW = 3;
     private static final int COL = 3;
@@ -32,10 +34,7 @@ public class TicTacToe {
     }
 
     private static boolean isValidMove(int row, int col){
-        if (board[row][col]==" "){
-            return true;
-        }
-        return false;
+        return board[row][col].equals(" ");
     }
 
     private static boolean isColWin(String player){
@@ -43,7 +42,7 @@ public class TicTacToe {
 
         for (int row = 0; row<ROW;row++){
             for (int col = 0; col<COL; col++){
-                if (board[col][row]==player){
+                if (board[col][row].equals(player)){
                     win += 1;
                 }
             }
@@ -62,7 +61,7 @@ public class TicTacToe {
 
         for (int col = 0; col<COL;col++){
             for (int row = 0; row<ROW; row++){
-                if (board[col][row]==player){
+                if (board[col][row].equals(player)){
                     win += 1;
                 }
             }
@@ -80,7 +79,7 @@ public class TicTacToe {
         int win = 0;
 
         for (int row = 0; row<ROW;row++){
-            if (board[row][row]==player){
+            if (board[row][row].equals(player)){
                 win += 1;
             }
         }
@@ -94,7 +93,7 @@ public class TicTacToe {
         for (int col = 0; col<COL;col++){
             row -= 1;
             //System.out.printf("board[col, %s][row, %s]%n",col,row);
-            if (board[col][row]==player){
+            if (board[col][row].equals(player)){
                 win += 1;
             }
         }
@@ -109,73 +108,166 @@ public class TicTacToe {
         return false;
     }
 
-    private static boolean isTie(){
-        int end = 0;
-        for (int col = 0; col < COL; col ++){
-            for (int row = 0; row < ROW; row++){
-                if (board[col][row].equals(" ")){
-                    end++;
-                    if (end == 9){
-                        return false;
-                    }
+    private static boolean isTie() {
+        boolean xFound;
+        boolean oFound;
+
+        // Check rows
+        for (int row = 0; row < ROW; row++) {
+            xFound = false;
+            oFound = false;
+            for (int col = 0; col < COL; col++) {
+                if (board[row][col].equals("X")) {
+                    xFound = true;
+                } else if (board[row][col].equals("O")) {
+                    oFound = true;
                 }
             }
-        }
-
-        for (int row = 0; row < ROW; row++) {
-            if (board[row][0].equals("X") && board[row][1].equals("O") && board[row][2].equals("X")) {
-                return false;
-            }
-            if (board[row][0].equals("O") && board[row][1].equals("X") && board[row][2].equals("O")) {
+            if (!xFound || !oFound) {
                 return false;
             }
         }
 
+        // Check columns
         for (int col = 0; col < COL; col++) {
-            if (board[0][col].equals("X") && board[1][col].equals("O") && board[2][col].equals("X")) {
+            xFound = false;
+            oFound = false;
+            for (int row = 0; row < ROW; row++) {
+                if (board[row][col].equals("X")) {
+                    xFound = true;
+                } else if (board[row][col].equals("O")) {
+                    oFound = true;
+                }
+            }
+            if (!xFound || !oFound) {
                 return false;
             }
-            if (board[0][col].equals("O") && board[1][col].equals("X") && board[2][col].equals("O")) {
-                return false;
+        }
+
+        xFound = false;
+        oFound = false;
+        for (int i = 0; i < ROW; i++) {
+            if (board[i][i].equals("X")) {
+                xFound = true;
+            } else if (board[i][i].equals("O")) {
+                oFound = true;
             }
         }
-
-        if (board[0][0].equals("X") && board[1][1].equals("O") && board[2][2].equals("X")) {
-            return false;
-        }
-        if (board[0][0].equals("O") && board[1][1].equals("X") && board[2][2].equals("O")) {
-            return false;
-        }
-        if (board[0][2].equals("X") && board[1][1].equals("O") && board[2][0].equals("X")) {
-            return false;
-        }
-        if (board[0][2].equals("O") && board[1][1].equals("X") && board[2][0].equals("O")) {
+        if (!xFound || !oFound) {
             return false;
         }
 
-        return true;
+        xFound = false;
+        oFound = false;
+        for (int i = 0; i < ROW; i++) {
+            if (board[i][ROW - 1 - i].equals("X")) {
+                xFound = true;
+            } else if (board[i][ROW - 1 - i].equals("O")) {
+                oFound = true;
+            }
+        }
+        return xFound && oFound;
     }
 
-    private static boolean isWin(String player){
-        if (isColWin(player)||isRowWin(player)||isDiagonalWin(player)){
-            return true;
-        }
-        return false;
+
+    private static boolean isWin(String player) {
+        return isColWin(player) || isRowWin(player) || isDiagonalWin(player);
     }
+
 
     public static void main(String[] args) {
+/*
+assuming that the pseudocode is only for outlining the main method and thus the other methods I've made will be used in the pseudocode
+
+boolean done = false
+num playerColMove
+num playerRow Move
+clearBoard()
+String layer = "X"
+boolean completed = false
+do
+    do
+        do
+            display()
+            output "Enter a row for " + player
+            input playerColMove
+            output "Enter a column for " + player
+            input playerRowMove
+            if isValidMove(playerColMove, playerRowMove) then
+                done = true
+            else
+                output "Enter a valid move to an unoccupied space."
+                done = false
+            end if
+        while !done
+        board[playerColMove][playerRowMove]=player
+        if player = "X" then
+            player = "O"
+        else
+            player = "X"
+        end if
+        if isWin("X") || isWin("O") || isTie() then
+            completed = true
+            if isWin("X") then
+                output "X wins!"
+            else if isWin("Y") then
+                output "Y wins!"
+            else if isTie() then
+                output "A tie!"
+            end if
+        end if
+    while !completed
+while SafeInput.getYNConfirm(in, "Would you like to play again?")
+ */
+        boolean done;
+        Scanner in = new Scanner(System.in);
+        int playerColMove;
+        int playerRowMove;
         clearBoard();
-        /*for (int col = 2; col > -1; col--){
-            board[col][col] = "X";
-        }*/
+        String player="X";
+        boolean completed = false;
 
-        for (int col = 0; col < COL; col ++){
-            for (int row = 0; row < ROW; row++){
-                board[row][col]="X";
-            }
-        }
+        do {
+            do {
+                do {
+                    display();
+                    playerColMove = SafeInput.getRangedInt(in,"Enter a row for " + player,1,3)-1;
+                    playerRowMove = SafeInput.getRangedInt(in,"Enter a column for " + player,1,3)-1;
+                    if (isValidMove(playerColMove, playerRowMove)){
+                        done = true;
+                    }
+                    else {
+                        System.out.println("Enter a valid move to an unoccupied space.");
+                        done = false;
+                    }
+                } while(!done);
 
-        display();
-        //System.out.println(isTie());
+                board[playerColMove][playerRowMove]=player;
+                if (player.equals("X")) {
+                    player = "O";
+                }
+                else {
+                    player = "X";
+                }
+
+                if(isWin("X")||isWin("O")||isTie()){
+                    completed=true;
+                    if (isWin("X")) {
+                        System.out.println("X wins!");
+                    }
+                    else if (isWin("O")){
+                        System.out.println("O wins!");
+                    }
+                    else if (isTie()){
+                        System.out.println("A tie!");
+                    }
+                    display();
+                }
+
+            } while(!completed);
+            clearBoard();
+            completed = false;
+            player = "X";
+        } while(SafeInput.getYNConfirm(in,"Would you like to play again?"));
     }
 }
